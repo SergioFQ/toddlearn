969b7fb83ea42a2e3385ff3290a0200b74d75e73
SergioFQ <sergio.fq25@gmail.com>
2019-12-03 21:37:32 +0100

    Added navigation from GameSelectionActivity to Game1Activity and Game2Activity. Also implemented the go back button.

--- app/src/main/java/com/g12/toddlearn/GameSelectionActivity.java
+++ app/src/main/java/com/g12/toddlearn/GameSelectionActivity.java
@@ -2,7 +2,10 @@ package com.g12.toddlearn;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class GameSelectionActivity extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class GameSelectionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_selection);
+    }
+
+
+    public void goBack(View v) {
+       onBackPressed();
+    }
+
+
+    public void goToG1(View view) {
+        startActivity(new Intent(this, Game1Activity.class));
+    }
+
+    public void goToG2(View view) {
+        startActivity(new Intent(this, Game2Activity.class));
     }
 }
