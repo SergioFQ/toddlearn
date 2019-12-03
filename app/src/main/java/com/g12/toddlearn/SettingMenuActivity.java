cb125e8f59d8f75f1f4adbd0b257357e7055f3b5
SergioFQ <sergio.fq25@gmail.com>
2019-12-03 22:12:07 +0100

    Added navigation from SettingsMenuActivity to DataChangeActivity and ChildProgressActivity. Also implemented the go back button.

--- app/src/main/java/com/g12/toddlearn/SettingMenuActivity.java
+++ app/src/main/java/com/g12/toddlearn/SettingMenuActivity.java
@@ -2,7 +2,9 @@ package com.g12.toddlearn;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class SettingMenuActivity extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class SettingMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting_menu);
+    }
+
+    public void goToChangePersonalData(View view) {
+        startActivity(new Intent(this,DataChangeActivity.class));
+    }
+
+    public void goToChildProgress(View view) {
+        startActivity(new Intent(this,ChildProgressActivity.class));
+    }
+
+    public void goBack(View view) {
+        onBackPressed();
     }
 }
