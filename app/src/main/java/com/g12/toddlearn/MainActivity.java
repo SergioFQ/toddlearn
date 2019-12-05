15c9234be6f98ce71975f7d2e871c7d49ee7edd3
albammiguel <am.miguel.2017@alumnos.urjc.es>
2019-12-05 18:45:53 +0100

    LOG IN POP UP CREATED + show and dismiss function implemented.

--- app/src/main/java/com/g12/toddlearn/MainActivity.java
+++ app/src/main/java/com/g12/toddlearn/MainActivity.java
@@ -2,20 +2,33 @@ package com.g12.toddlearn;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Dialog myDialog; //for the pop-up
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myDialog = new Dialog(this);
     }
 
     public void goToSelGame(View view) {
         //Intent selection = new Intent(this,GameSelectionActivity.class);
         startActivity(new Intent(this, GameSelectionActivity.class));
     }
+
+    public void showPopUp(View view){
+
+        myDialog.setContentView(R.layout.login_pop_up);
+        Button close_button = myDialog.findViewById(R.id.close_button);
+        myDialog.show();
+    }
+
+    public void closePopUp(View view){ myDialog.dismiss(); }
+
 }
